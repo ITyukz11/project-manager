9962f034ed4187666a9462452cb5cfe0e1f947f8
ITYukz11 <it.era0211@gmail.com>
2025-11-23 20:08:21 +0800

    fix: enhance error handling and loading state in cash-out page

--- src/app/(app)/cash-outs/page.tsx
+++ src/app/(app)/cash-outs/page.tsx
@@ -4,19 +4,52 @@ import { useCashouts } from "@/lib/hooks/swr/cashout/useCashouts";
 import { DataTable } from "@/components/table/data-table";
 import { cashoutColumns } from "@/components/table/cashout/cashoutColumns";
 import { useRouter } from "next/navigation";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Skeleton } from "@/components/ui/skeleton";
+import { TriangleAlert } from "lucide-react";
 
 const Page = () => {
-  const { cashouts } = useCashouts();
+  const { cashouts, error, isLoading } = useCashouts();
 
   const router = useRouter();
   return (
     <div>
-      <DataTable
-        data={cashouts}
-        columns={cashoutColumns}
-        cursorRowSelect
-        onViewRowId={(id) => router.push("/cash-outs/" + id)}
-      />
+      <div className="flex items-center justify-between">
+        <div className="flex flex-row gap-2 ml-auto">
+          {error && (
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <TriangleAlert className="text-red-500" />
+              </TooltipTrigger>
+              <TooltipContent side="bottom" sideOffset={6} className="max-w-xs">
+                <div className="text-sm text-red-400 dark:text-red-700">
+                  {error?.message ||
+                    "Error loading accounts. Please try again later."}
+                </div>
+              </TooltipContent>
+            </Tooltip>
+          )}
+        </div>
+      </div>
+
+      {/* Table */}
+      {isLoading ? (
+        <div className="w-full flex flex-col gap-2 items-center">
+          <Skeleton className="w-full h-10" />
+          <Skeleton className="w-full h-40" />
+        </div>
+      ) : (
+        <DataTable
+          data={cashouts}
+          columns={cashoutColumns}
+          cursorRowSelect
+          onViewRowId={(id) => router.push("/cash-outs/" + id)}
+        />
+      )}
     </div>
   );
 };
