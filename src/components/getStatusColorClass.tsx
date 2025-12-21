82bb0c79c258e7cd20e4239491356ade60e81c1a
ITYukz11 <it.era0211@gmail.com>
2025-12-21 22:01:09 +0800

    Refactor code structure for improved readability and maintainability

--- src/components/getStatusColorClass.tsx
+++ src/components/getStatusColorClass.tsx
@@ -1,19 +1,32 @@
-import { AlertCircle, BadgeCheck, CheckCircle2, XCircle } from "lucide-react";
+import {
+  AlertCircle,
+  BadgeCheck,
+  CheckCircle2,
+  Circle,
+  XCircle,
+} from "lucide-react";
 import { Spinner } from "./ui/spinner";
 
 export function getStatusColorClass(status: string): string {
   switch (status) {
     case "PENDING":
-      return "bg-yellow-600 hover:bg-yellow-700 text-white";
+      return "bg-yellow-600 hover:bg-yellow-700 text-white ";
+
     case "PARTIAL":
-      return "bg-blue-600 hover:bg-blue-700 text-white";
+      return "bg-sky-600 hover:bg-sky-700 text-white";
+
     case "COMPLETED":
     case "APPROVED":
       return "bg-green-600 hover:bg-green-700 text-white";
+
     case "CLAIMED":
       return "bg-blue-600 hover:bg-blue-700 text-white";
-    default:
+
+    case "REJECTED":
       return "bg-red-600 hover:bg-red-700 text-white";
+
+    default:
+      return "bg-gray-600 hover:bg-gray-700 text-white";
   }
 }
 
@@ -22,12 +35,19 @@ export const getStatusIcon = (status?: string) => {
     case "APPROVED":
     case "COMPLETED":
       return <BadgeCheck className="h-4 w-4" />;
+
     case "CLAIMED":
       return <CheckCircle2 className="h-4 w-4" />;
-    case "REJECTED":
-      return <XCircle className="h-4 w-4" />;
+
+    case "PARTIAL":
+      return <AlertCircle className="h-4 w-4 animate-pulse" />;
+
     case "PENDING":
       return <Spinner />;
+
+    case "REJECTED":
+      return <XCircle className="h-4 w-4" />;
+
     default:
       return <AlertCircle className="h-4 w-4" />;
   }
