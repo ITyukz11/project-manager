09ad7b5848e8c3b53fcee2be5da10e706fd735d3
ITYukz11 <it.era0211@gmail.com>
2025-12-30 20:45:07 +0800

    feat: enhance attendance logs page with user role handling and error display

--- src/app/(app)/attendance-logs/page.tsx
+++ src/app/(app)/attendance-logs/page.tsx
@@ -2,35 +2,33 @@
 
 import { Card, CardContent } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
-
-// Import your DataTable and column definition (create these for casino groups!)
 import { DataTable } from "@/components/table/data-table";
 import { useSession } from "next-auth/react";
 import { useAttendanceLogs } from "@/lib/hooks/swr/attendance/useAttendanceLogs";
 import { TriangleAlert } from "lucide-react";
 import { attendanceColumns } from "@/components/table/attendance/attendanceColumns";
+import { ADMINROLES } from "@/lib/types/role";
 
 export default function AttendanceLogsPage() {
   const { data: session } = useSession();
 
-  const { attendanceLogs, attendanceError, attendanceLoading } =
-    useAttendanceLogs();
+  const isAdmin =
+    session?.user?.role === ADMINROLES.SUPERADMIN ||
+    session?.user?.role === ADMINROLES.ADMIN;
 
-  // Customize columns to hide if needed
-  const hiddenColumns: string[] = [];
-  console.log("session:", session);
+  const userId = !isAdmin ? session?.user?.id : undefined;
+
+  const { attendanceLogs, attendanceError, attendanceLoading } =
+    useAttendanceLogs(userId ?? undefined);
+
   return (
     <Card>
       <CardContent>
         <div className="flex items-center justify-between mb-6">
           <h1 className="text-3xl font-bold">Attendance Logs</h1>
-
-          <div className="flex flex-row gap-2 items-center">
-            {attendanceError && <TriangleAlert className="text-red-500" />}
-          </div>
+          {attendanceError && <TriangleAlert className="text-red-500" />}
         </div>
 
-        {/* Table/List */}
         {attendanceLoading ? (
           <div className="w-full flex flex-col gap-2 items-center">
             <Skeleton className="w-full h-10" />
@@ -38,16 +36,12 @@ export default function AttendanceLogsPage() {
           </div>
         ) : (
           <DataTable
-            data={attendanceLogs || []}
+            data={attendanceLogs ?? []}
             columns={attendanceColumns}
             allowSelectRow={false}
-            hiddenColumns={hiddenColumns}
+            hiddenColumns={[]}
             cursorRowSelect
             allowExportData={true}
-            // onViewRowId={(id) => {
-            //   setCasinoRowId(id);
-            //   setEditOpen(true);
-            // }}
           />
         )}
       </CardContent>
