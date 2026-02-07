011c8d50c5037958151c913fb35f8879a873dc5d
ITYukz11 <it.era0211@gmail.com>
2026-02-07 20:43:12 +0800

    fix: change output option from export to standalone in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone", // ensures all assets are properly versioned
   reactCompiler: false,
-  output: "export",
   images: {
     remotePatterns: [
       {
@@ -13,6 +13,19 @@ const nextConfig: NextConfig = {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*", // matches all routes
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache", // ensures HTML pages are always fresh
+          },
+        ],
+      },
+    ];
   },
 };
 
